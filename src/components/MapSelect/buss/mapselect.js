1ebb8b1df19552663be179ee1c2dc0bbd7a20ba7
wqy <wqy224488@163.com>
2023-03-06 15:19:51 +0800

    program update

--- src/components/MapSelect/buss/mapselect.js
+++ src/components/MapSelect/buss/mapselect.js
@@ -308,6 +308,8 @@ export function ToggleTimeShow(visible) {
 
 export function ToggleDepthWMShow(visible) {
     if (cesiumviewer.CesiumViewer.getInstance().depthimagery == null) return;
+    const viewer = cesiumviewer.CesiumViewer.getInstance().viewer;
+    if (viewer == undefined) return;
 
     cesiumviewer.CesiumViewer.getInstance().depthimagery.show = visible;
     if (visible === true) {
@@ -317,6 +319,8 @@ export function ToggleDepthWMShow(visible) {
 
 export function ToggleUnderSeaFeatureShow(visible) {
     if (cesiumviewer.CesiumViewer.getInstance().underseafeatureimagery == null) return;
+    const viewer = cesiumviewer.CesiumViewer.getInstance().viewer;
+    if (viewer == undefined) return;
 
     cesiumviewer.CesiumViewer.getInstance().underseafeatureimagery.show = visible;
     if (visible === true) {
@@ -335,7 +339,7 @@ export function ToggleOceanWaterShow(visible) {
                     rectangle: Cesium.Rectangle.fromDegrees(-180.0, -90.0, 180.0, 90.0),
                     vertexFormat: Cesium.EllipsoidSurfaceAppearance.VERTEX_FORMAT,
                     height: 0,
-                    // extrudedHeight: 1,
+                    extrudedHeight: 1,
                     // shadowVolume: true,
                 })
                 // geometry: Cesium.RectangleGeometry.createShadowVolume(new Cesium.RectangleGeometry({
@@ -428,8 +432,87 @@ czm_material czm_getMaterial(czm_materialInput materialInput)
 }
 
                         `
-                    },
+                    }
                 }),
+                // flat:true,
+//                 material: new Cesium.Material({
+//                     fabric: {
+//                         // type: "Water",
+//                         uniforms: {
+//                             baseWaterColor: new Cesium.Color(64 / 255.0, 127 / 255.0, 253 / 255.0, 0.1),
+//                             blendColor: new Cesium.Color(64 / 255.0, 157 / 255.0, 253 / 255.0, 0.1),
+//                             specularMap: '/images/cesium/earthspec1k.jpg',
+//                             normalMap: '/images/cesium/waterNormals.jpg',
+//                             frequency: 1000.0,
+//                             animationSpeed: 0.02,
+//                             amplitude: 10,
+//                             specularIntensity: 5,
+//                             fadeFactor: 1.0,
+//                         },
+//                         source: `
+//                         // Thanks for the contribution Jonas
+// // http://29a.ch/2012/7/19/webgl-terrain-rendering-water-fog
+//
+// uniform sampler2D specularMap;
+// uniform sampler2D normalMap;
+// uniform vec4 baseWaterColor;
+// uniform vec4 blendColor;
+// uniform float frequency;
+// uniform float animationSpeed;
+// uniform float amplitude;
+// uniform float specularIntensity;
+// uniform float fadeFactor;
+//
+// czm_material czm_getMaterial(czm_materialInput materialInput)
+// {
+//     czm_material material = czm_getDefaultMaterial(materialInput);
+//
+//     float time = czm_frameNumber * animationSpeed;
+//
+//     // fade is a function of the distance from the fragment and the frequency of the waves
+//     float fade = max(1.0, (length(materialInput.positionToEyeEC) / 1000000.0) * frequency * fadeFactor);
+//
+//     float specularMapValue = texture2D(specularMap, materialInput.st).r;
+//
+//     // note: not using directional motion at this time, just set the angle to 0.0;
+//     vec4 noise = czm_getWaterNoise(normalMap, materialInput.st * frequency, time, 0.0);
+//     vec3 normalTangentSpace = noise.xyz * vec3(1.0, 1.0, (1.0 / amplitude));
+//
+//     // fade out the normal perturbation as we move further from the water surface
+//     normalTangentSpace.xy /= fade;
+//
+//     // attempt to fade out the normal perturbation as we approach non water areas (low specular map value)
+//     normalTangentSpace = mix(vec3(0.0, 0.0, 50.0), normalTangentSpace, specularMapValue);
+//
+//     normalTangentSpace = normalize(normalTangentSpace);
+//
+//     // get ratios for alignment of the new normal vector with a vector perpendicular to the tangent plane
+//     float tsPerturbationRatio = clamp(dot(normalTangentSpace, vec3(0.0, 0.0, 1.0)), 0.0, 1.0);
+//
+//     // fade out water effect as specular map value decreases
+//     material.alpha = mix(blendColor.a, baseWaterColor.a, specularMapValue) * specularMapValue;
+//
+//
+//     // base color is a blend of the water and non-water color based on the value from the specular map
+//     // may need a uniform blend factor to better control this
+//     material.diffuse = mix(blendColor.rgb, baseWaterColor.rgb, specularMapValue);
+//
+//     // diffuse highlights are based on how perturbed the normal is
+//     material.diffuse += (0.1 * tsPerturbationRatio);
+//
+//     material.diffuse = material.diffuse;
+//
+//     material.normal = normalize(materialInput.tangentToEyeMatrix * normalTangentSpace);
+//
+//     material.specular = specularIntensity;
+//     material.shininess = 10.0;
+//
+//     return material;
+// }
+//
+//                         `
+//                     },
+//                 }),
                 aboveGround: true,
             }),
             show: true,
